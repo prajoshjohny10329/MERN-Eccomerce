5201ab053d0f4c489c8e1e21a92392344660c470
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-10-20 21:52:02 +0530

    add basic on signup

--- client/src/pages/auth/SignUp.jsx
+++ client/src/pages/auth/SignUp.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const SignUp = () => {
+const initialState = {
+  userName: '',
+  email: '',
+  password: ''
+}
+
+const SignUp = () => { 
+  const [formData, setFormData] = useState(initialState)
   return (
-    <div>SignUp</div>
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+      <h1 className="text-3xl font-bold -tracking-tight text-foreground">Create new Account</h1>
+      <p className='mt-2 '>Already Have an Account</p>
+      <Link className='font-medium ml-2 text-purple-500 hover:underline' to={'auth/login'}></Link>
+      </div>
+    </div>
   )
 }
 

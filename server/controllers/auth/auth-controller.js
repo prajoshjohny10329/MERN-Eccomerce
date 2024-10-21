75391cb8a642151d6416a0fc865ab3300b8319e1
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-10-21 22:40:12 +0530

    add dummy constroler for user

---
+++ server/controllers/auth/auth-controller.js
@@ -0,0 +1,11 @@
+
+
+const register =  async (req,res) =>{
+    // const {name, email , phone, password} =req.body
+    console.log('req.body');
+    console.log(req.body);
+    
+
+}
+
+module.exports = { register }

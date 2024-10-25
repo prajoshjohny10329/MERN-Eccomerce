6e7f16aa6d51e22b71295cd387024be859d79f37
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-10-26 00:34:07 +0530

    IF Authenticated user Access Admin pages

--- client/src/components/common/CheckAuth.jsx
+++ client/src/components/common/CheckAuth.jsx
@@ -3,46 +3,41 @@ import { Navigate, useLocation } from "react-router-dom";
 
 const CheckAuth = ({ isAuthenticated, user, children }) => {
   //return current url location
-  console.log('check auth called');
-  
-  const urlLocation = useLocation();
-  const redirectAfterLogin = sessionStorage.getItem("redirectAfterLogin")
-  console.log(redirectAfterLogin);
-  
+  console.log("check auth called");
 
-  // Manage for IF unAuthenticated user 
+  const urlLocation = useLocation();
+  const redirectAfterLogin = sessionStorage.getItem("redirectAfterLogin");
+
+  // Manage for IF unAuthenticated user with store current url
   if (
     !isAuthenticated &&
     !(
       urlLocation.pathname.includes("/login") ||
-      urlLocation.pathname.includes("/sign-up") 
+      urlLocation.pathname.includes("/sign-up")
     )
   ) {
-    
     sessionStorage.setItem("redirectAfterLogin", urlLocation.pathname);
     return <Navigate to={"/auth/login"} />;
   }
 
-  if ( isAuthenticated && redirectAfterLogin ) {
+  // Manage for After login unAuthenticated user redirect old url
+  if (isAuthenticated && redirectAfterLogin) {
     sessionStorage.removeItem("redirectAfterLogin");
     return <Navigate to={redirectAfterLogin} replace />;
   }
 
-
-  // sessionStorage.getItem("redirectAfterLogin") 
-  // sessionStorage.setItem("redirectAfterLogin", urlLocation.pathname);
-  // sessionStorage.removeItem("redirectAfterLogin");
-
-  // logic for Not Authenticated User for ignore !Authentication Pages
-
-
-  
+  // IF Authenticated user Access Admin pages
+  if (isAuthenticated && urlLocation.pathname.includes("/admin") && user?.role === "user") {
+    console.log('user is not');
+    
+      return <Navigate to={"/"} />;
+  }
 
   // logic for Authenticated User for ignore Admin Pages
   if (isAuthenticated && urlLocation.pathname.includes("/admin")) {
     if (user?.role === "user") {
-      console.log('if2');
-      
+      console.log("if2");
+
       return <Navigate to={"/"} />;
     }
   }
@@ -51,22 +46,24 @@ const CheckAuth = ({ isAuthenticated, user, children }) => {
   // if(isAuthenticated && user.role === 'admin'){
   //   return <Navigate to={"/admin"} />;
   // }
-  
+
   // logic for Authenticated User or admin To access admin redirect own way
-  if (isAuthenticated && urlLocation.pathname.includes("/auth" || "/auth/login" || "/auth/sign-up")) {
+  if (
+    isAuthenticated &&
+    urlLocation.pathname.includes("/auth" || "/auth/login" || "/auth/sign-up")
+  ) {
     if (user?.role === "admin") {
-      console.log('if3');
+      console.log("if3");
 
       return <Navigate to={"/admin"} />;
-    }else{
-      console.log('if4');
+    } else {
+      console.log("if4");
 
-        return <Navigate  to={"/"} />;
+      return <Navigate to={"/"} />;
     }
   }
 
-
-  return <>{children}</>
+  return <>{children}</>;
 };
 
 export default CheckAuth;
--- client/src/components/common/CheckAuth.md
+++ client/src/components/common/CheckAuth.md
@@ -4,6 +4,13 @@ IF unAuthenticated user Access only
         product list
         single product
 
+IF unAuthenticated user Access Authenticated pages After login Redirect old URL
+    ex:
+        unAuthenticatedUser ===> checkout ===> login ===>  AuthenticatedUser  ===>  checkout
+
+
 IF Authenticated user Access Admin pages
+    ex:
+        AuthenticatedUser  ===>  adminPages  ==>  userURL
 
 IF

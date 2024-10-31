16caa488496301c3e235d4f20ca56aa45170db79
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-11-01 00:29:57 +0530

    add admin side bar products

--- client/src/components/admin/AdminSideBar.jsx
+++ client/src/components/admin/AdminSideBar.jsx
@@ -1,4 +1,4 @@
-import { ChartArea } from 'lucide-react'
+import { Boxes, ChartArea } from 'lucide-react'
 import React, { Fragment } from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -8,9 +8,17 @@ const AdminSideBar = () => {
   return (
     <Fragment>
       <aside className="hidden w-64 flex-col border-r bg-transparent p-6 lg:flex">
-        <div onClick={()=> navigate('/admin/dashboard')} className="flex cursor-pointer items-center gap-2">
+
+        {/* Navigate to Admin Panel  */}
+        <div onClick={()=> navigate('/admin/dashboard')} className="flex cursor-pointer items-center gap-2 py-3">
           <ChartArea /> 
           <h1 className='text-lg font-bold'>Admin Panel</h1>
+        </div>
+
+        {/* Navigate to products  */}
+        <div onClick={()=> navigate('/admin/products')} className="flex cursor-pointer items-center gap-2 py-3">
+        <Boxes /> 
+          <h1 className='text-lg font-bold'>Our Products</h1>
         </div>
       </aside>
     </Fragment>

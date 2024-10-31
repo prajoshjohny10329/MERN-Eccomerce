c293782cad28e8175e21a53f2b8307daf74fc9ae
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-11-01 00:25:34 +0530

    add admin side bar base

--- client/src/components/admin/AdminSideBar.jsx
+++ client/src/components/admin/AdminSideBar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { ChartArea } from 'lucide-react'
+import React, { Fragment } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AdminSideBar = () => {
+
+  const navigate = useNavigate();
   return (
-    <div>AdminSideBar</div>
+    <Fragment>
+      <aside className="hidden w-64 flex-col border-r bg-transparent p-6 lg:flex">
+        <div onClick={()=> navigate('/admin/dashboard')} className="flex cursor-pointer items-center gap-2">
+          <ChartArea /> 
+          <h1 className='text-lg font-bold'>Admin Panel</h1>
+        </div>
+      </aside>
+    </Fragment>
   )
 }
 

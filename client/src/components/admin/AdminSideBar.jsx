38d0a53e08ccf29ae0b75eebf5d898d945202f99
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-11-02 19:54:36 +0530

    implement dynamic sidebar

--- client/src/components/admin/AdminSideBar.jsx
+++ client/src/components/admin/AdminSideBar.jsx
@@ -1,25 +1,71 @@
-import { Boxes, ChartArea } from 'lucide-react'
+// import { adminSidebarItems } from '@/config/adminSidebarItems'
+import { Boxes, ChartArea, LayoutDashboard } from 'lucide-react'
 import React, { Fragment } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const AdminSideBar = () => {
+const adminSidebarItems = [
+  {
+      id: 'dashboard',
+      label: 'Dashboard',
+      path: '/admin/dashboard',
+      icon: <LayoutDashboard />
+  },
+  {
+      id: 'products',
+      label: 'Products',
+      path: '/admin/products',
+      icon: <Boxes />
+  },
+  {
+      id: 'orders',
+      label: 'Orders',
+      path: '/admin/orders',
+      icon: <ChartArea />
+  }
+]
 
+const DynamicSidebar = () =>{
   const navigate = useNavigate();
+  return (
+    <nav className='mt-8 flex flex-col'>
+      {
+        adminSidebarItems.map(menuItem =>(
+          <div key={menuItem.id} onClick={()=> navigate(menuItem.path)} className="flex cursor-pointer items-center gap-2 py-3">
+            {menuItem.icon}
+            {
+              console.log(menuItem.icon)
+              
+            }
+            <div className='text-lg font-bold'>{menuItem.label}</div>
+          </div>
+        ))
+      }
+      {/* <div onClick={()=> navigate('/admin/dashboard')} className="flex cursor-pointer items-center gap-2 py-3">
+          <ChartArea /> 
+          <h1 className='text-lg font-bold'>Admin Panel</h1>
+        </div> */}
+
+    </nav>
+  )
+}
+
+const AdminSideBar = () => {
   return (
     <Fragment>
       <aside className="hidden w-64 flex-col border-r bg-transparent p-6 lg:flex">
+        <DynamicSidebar />
 
         {/* Navigate to Admin Panel  */}
-        <div onClick={()=> navigate('/admin/dashboard')} className="flex cursor-pointer items-center gap-2 py-3">
+        {/* <div onClick={()=> navigate('/admin/dashboard')} className="flex cursor-pointer items-center gap-2 py-3">
           <ChartArea /> 
           <h1 className='text-lg font-bold'>Admin Panel</h1>
-        </div>
+        </div> */}
 
         {/* Navigate to products  */}
-        <div onClick={()=> navigate('/admin/products')} className="flex cursor-pointer items-center gap-2 py-3">
+        {/* <div onClick={()=> navigate('/admin/products')} className="flex cursor-pointer items-center gap-2 py-3">
         <Boxes /> 
           <h1 className='text-lg font-bold'>Our Products</h1>
-        </div>
+        </div> */}
       </aside>
     </Fragment>
   )
--- client/src/config/adminSidebarItems.js
+++ client/src/config/adminSidebarItems.js
@@ -1,14 +1,22 @@
+import { Boxes, ChartArea } from 'lucide-react'
+
 export const adminSidebarItems = [
     {
         id: 'dashboard',
         label: 'Dashboard',
         path: '/admin/dashboard',
-        icon: '<ChartArea />'
+        icon: <ChartArea />
+    },
+    {
+        id: 'products',
+        label: 'Products',
+        path: '/admin/products',
+        icon: <ChartArea />
     },
     {
         id: 'orders',
         label: 'Orders',
         path: '/admin/dashboard',
-        icon: '<Boxes />'
+        icon: <Boxes />
     }
-]
+  ]

9e823c4a5db01e150a662ee7d2369e1ee82ed296
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-11-09 10:28:02 +0530

    add function for drag and drop

--- client/src/components/admin/product/ProductImageUpload.jsx
+++ client/src/components/admin/product/ProductImageUpload.jsx
@@ -11,15 +11,25 @@ const ProductImageUpload = ({
 }) => {
   const inputRef = useRef(null);
 
+  //handel file change
   const handelImageFileChange = (event) => {
     console.log("handel change");
     const imageFile = event.target.files?.[0];
     if (imageFile) setImageFile(imageFile);
   };
+  //for handelOnDrag
+  const handelOnDrag = event =>  event.preventDefault()
+
+  //for handelOnDrop
+  const handelOnDrop = (event) =>{
+    event.preventDefault()
+    const dropImageFile = event.dataTransfer.files?.[0];
+    if(dropImageFile)  setImageFile(dropImageFile)
+  }
   return (
     <div className="w-full mt-1">
       <Label className="mb-1">Product Image Upload</Label>
-      <div className="border-2 p-4 mt-4 border-dashed rounded-lg">
+      <div onDragOver={handelOnDrag} onDrop={handelOnDrop} className="border-2 p-4 mt-4 border-dashed rounded-lg">
         <Input
           id="image-upload"
           className="py-6 hidden"

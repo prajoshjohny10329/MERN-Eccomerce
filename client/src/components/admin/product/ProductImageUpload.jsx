6ba37ce0b89d6398c5f1574bbba2b021a49414c0
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-11-09 10:12:45 +0530

    add UploadCloudIcon componnet

--- client/src/components/admin/product/ProductImageUpload.jsx
+++ client/src/components/admin/product/ProductImageUpload.jsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { UploadCloudIcon } from "lucide-react";
 import React, { useRef } from "react";
 
 const ProductImageUpload = ({
@@ -10,21 +11,30 @@ const ProductImageUpload = ({
 }) => {
   const inputRef = useRef(null);
 
-  const handelImageFileChange = (event) =>{
-    console.log('handel change');
-    const imageFile  = event.target.files?.[0]
-    if(imageFile) setImageFile(imageFile);
-      console.log(imageFile);
-      
-    
-    
-
-  }
+  const handelImageFileChange = (event) => {
+    console.log("handel change");
+    const imageFile = event.target.files?.[0];
+    if (imageFile) setImageFile(imageFile);
+  };
   return (
     <div className="w-full mt-1">
       <Label className="mb-1">Product Image Upload</Label>
-      <div>
-        <Input id="image-upload" className="py-6" type="file" ref={inputRef} onChange={handelImageFileChange} />
+      <div className="border-2 p-4 mt-4 border-dashed rounded-lg">
+        <Input
+          id="image-upload"
+          className="py-6 hidden"
+          type="file"
+          ref={inputRef}
+          onChange={handelImageFileChange}
+        />
+        {!imageFile ? (
+          <Label htmlFor="image-upload" className="flex flex-col items-center">
+            <UploadCloudIcon className="w-10 h-10 mb-2"></UploadCloudIcon>
+            <span>Drag and Drop or click to upload</span>
+          </Label>
+        ) : (
+          <div></div>
+        )}
       </div>
     </div>
   );

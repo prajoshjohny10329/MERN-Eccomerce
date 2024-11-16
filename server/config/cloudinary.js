a60a15e2cdc9cfc28bc0f18db0eb19bb457099fe
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-11-16 22:10:12 +0530

    add imageUploadUtils and multer config

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,7 +1,22 @@
 const cloudinary = require('cloudinary');
+const multer = require('multer')
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const storage = new multer.memoryStorage();
+
+const imageUploadUtils = async (file) =>{
+    try {
+        const uploadResult = await cloudinary.uploader.upload(file, { resource_type: 'auto' })
+        return uploadResult
+    } catch (error) {
+        console.log(error, 'error in uploadImage');
+    }
+}
+
+const upload = multer({multer})
+module.exports = {upload, imageUploadUtils}
